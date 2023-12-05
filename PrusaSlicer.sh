3a6372875e3e1cdc46c154d63c5e52e078162860
jablain <blain.jacques@gmail.com>
2023-12-05 15:51:33 -0500

    Backup

--- PrusaSlicer.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash 
-~/3DPrinting/Slicers/Prusa/PrusaSlicer.AppImage --datadir ~/3DPrinting/Slicers/Prusa/Projects/Settings
---
+++ bimboPrusaSlicer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+~/3DPrinting/Slicers/Prusa/PrusaSlicer.AppImage --datadir ~/3DPrinting/Slicers/Prusa/Projects/Settings
